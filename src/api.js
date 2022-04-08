8f35f3a9f11cfae903658a1890bb0794c163a002
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-08 19:01:03 +0300

    dfg

--- src/api.js
+++ src/api.js
@@ -117,11 +117,9 @@ const createAuth = (req, res) => {
 const initChallenge = (req, res) => {
   const randomChallengeStr = uuidv4()
   const id = uuidv4()
-  const bufferedChallenge = encode(randomChallengeStr)
-  // const encodedChallenge = encode(randomChallengeStr)
   const newItem = {
     searchId: id,
-    challenge: bufferedChallenge,
+    challenge: randomChallengeStr,
   }
   userKeys.push(newItem)
   res.json(randomChallengeStr)
