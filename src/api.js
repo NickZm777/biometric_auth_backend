640fc72a7d813b836cc10e30a40cb086d1bea76e
Nick <lacoste777v@gmail.com>
2022-04-09 20:59:26 +0300

    erwerr

--- src/api.js
+++ src/api.js
@@ -110,6 +110,9 @@ const checkCreds = (req, res) => {
 const createAuth = (req, res) => {
   const object = req.body;
   object.response.clientDataJSON = JSON.parse(object.response.clientDataJSON);
+  object.response.clientDataJSON.challenge = btoa(
+    object.response.clientDataJSON.challenge
+  );
   userKeys.push(object);
   res.json(userKeys);
 };
