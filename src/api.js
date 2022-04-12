ef00414438aad385c4d90e5cb2485aeeeaa430c5
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-12 15:31:30 +0300

    asdddd

--- src/api.js
+++ src/api.js
@@ -12,26 +12,6 @@ function parseAttestationObject(attestationObject) {
   return cbor.decodeAllSync(buffer)[0]
 }
 
-// const strParse =
-//   '�cfmtdnonegattStmt�hauthDataX�5��;p-\u0016�%\u000c�7`��)�cg�P$%R��\u0007 ��Z6E\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0014�Vm7�\\\u0017�����%��O*s~�\u0001\u0002\u0003& \u0001!X f�nA�nA�\u0001��٫��w�\u0018d\u0001������5)\\��g"X ���2�\u0013b-��Hcc(�\u000c�#�]�<�F��)\u001d�CO'
-// const tst =
-//   "o2NmbXRkbm9uZWdhdHRTdG10oGhhdXRoRGF0YVjszHUM-fXe8fPTc7IQdAU8xhonRmZeDznRqJqecdVRcUNFYfOzo63OAAI1vMYKZIsLJfHwVQMAaAGnedEs8u2RW_H-8HXzJhTtnVHUAfErTK2AW4Saa0wiSClXWyIjLPXQAEyjr1KaCn5soeutmbDtSeT0FLIvcijbpg0fmQ-MHrw2GZ8Ka8rRn-a5-sncsUELQWD0sEvLttxXVQcQah2vpQECAyYgASFYIMG7Y3fOeGecLpfn7XF_sV4OTc41tsbEPSECGfCiK480IlggH9-qVehm6Gj25SyZau17mB5c0YoTWBZ8ngdEka4EqOY"
-
-// const ddd =
-//   "CSLvv71jZm10ZG5vbmVnYXR0U3RtdO+/vWhhdXRoRGF0YVjvv70177+977+9O3AtXHUwMDE277+9JVx1MDAwY++/vTdg77+977+9Ke+/vWNn77+9UCQlUu+/ve+/vVx1MDAwNyDvv73vv71aNkVcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMDBcdTAwMTTvv71WbTfvv71cXFx1MDAxN++/ve+/ve+/ve+/ve+/vSXvv73vv71PKnN+77+9XHUwMDAxXHUwMDAyXHUwMDAzJiBcdTAwMDEhWCBm77+9bkHvv71uQe+/vVx1MDAwMe+/ve+/vdmr77+977+9d++/vVx1MDAxOGRcdTAwMDHvv73vv73vv73vv73vv73vv701KVxc77+977+9Z1wiWCDvv73vv73vv70y77+9XHUwMDEzYi3vv73vv71IY2Mo77+9XHUwMDBj77+9I++/vV3vv70877+9Ru+/ve+/vSlcdTAwMWTvv71DTyI="
-
-// console.log(ArrayBuffer.isView(ddd))
-
-// const parseAttestation = parseAttestationObject(tst)
-
-// console.log(parseAttestation)
-
-// const fileBuffer =
-//   '�cfmtdnonegattStmt�hauthDataX�5��;p-\u0016�%\u000c�7`��)�cg�P$%R��\u0007 ��Z6E\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0000\u0014�\u0005���V�\u0017�\\y���8����3�\u0001\u0002\u0003& \u0001!X %� \u001a\u0014�X\u0003t8K�\\\u000f���u�mT��\u001dD�Vn~Đ\u000f"X Փ�\u0008\u001d\u007fU�� AB\u0014+g�qQ�\u0014*\u000f�Ť�Z\u0004@��'
-// const charsetMatch = detectCharacterEncoding(fileBuffer)
-
-// console.log(charsetMatch)
-
 const decode = (buffer, utf) => {
   return new TextDecoder(utf).decode(buffer)
 }
@@ -138,16 +118,24 @@ const checkCreds = (req, res) => {
 }
 
 const createAuth = (req, res) => {
+  const result = {
+    id: "randomChallengeStr",
+    challenge: "",
+    counter: 0,
+    publicKey: "",
+    attestationObject: "",
+    clientDataJSON: "",
+    userAgent: "",
+    user: "",
+  }
   const object = req.body
   // object.rawIdconverted = decode(object.rawId, "utf-8")
-
-  object.response.clientDataJSON = JSON.parse(
+  result.publicKey = object.id
+  result.clientDataJSON = JSON.parse(
     base64.decode(object.response.clientDataJSON)
   )
-  object.response.clientDataJSON.challengeer = base64.decode(
-    object.response.clientDataJSON.challenge
-  )
-  object.response.isEqual =
+  result.challenge = base64.decode(object.response.clientDataJSON.challenge)
+  result.isEqual =
     object.response.clientDataJSON.challengeer === randomChallengeStr
       ? true
       : false
@@ -156,11 +144,11 @@ const createAuth = (req, res) => {
   //   object.response.attestationObject
   // )
   // object.response.parsedAttObject = parsedAttestObj
-  object.response.attest = parseAttestationObject(
+  result.attestationObject = parseAttestationObject(
     object.response.attestationObject
   )
 
-  userKeys.push(object)
+  userKeys.push(result)
 
   res.json(userKeys)
 }
