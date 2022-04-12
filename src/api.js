792cbbb8c0f8e6798bc2c9ee963616b18e20c70e
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-12 15:48:17 +0300

    t

--- src/api.js
+++ src/api.js
@@ -118,56 +118,60 @@ const checkCreds = (req, res) => {
 }
 
 const createAuth = (req, res) => {
-  // const checkUser = userData.find((user) => user.login === reqLogin)
-  const result = {
-    id: "randomChallengeStr",
-    challenge: "",
-    counter: 0,
-    publicKey: "",
-    attestationObject: "",
-    clientDataJSON: "",
-    userAgent: "",
-    user: "",
+  try {
+    // const checkUser = userData.find((user) => user.login === reqLogin)
+    const result = {
+      id: "randomChallengeStr",
+      challenge: "",
+      counter: 0,
+      publicKey: "",
+      attestationObject: "",
+      clientDataJSON: "",
+      userAgent: "",
+      user: "",
+    }
+    const object = req.body
+    // object.rawIdconverted = decode(object.rawId, "utf-8")
+    // result.publicKey = object.id
+    result.clientDataJSON = JSON.parse(
+      base64.decode(object.response.clientDataJSON)
+    )
+    result.challenge = base64.decode(object.response.clientDataJSON.challenge)
+    result.isEqual = result.challenge === result.id ? true : false
+
+    // const parsedAttestObj = parseAttestationObject(
+    //   object.response.attestationObject
+    // )
+    // object.response.parsedAttObject = parsedAttestObj
+    result.attestationObject = parseAttestationObject(
+      object.response.attestationObject
+    )
+
+    userKeys.push(result)
+
+    // object.response.clientDataJSON = JSON.parse(
+    //   base64.decode(object.response.clientDataJSON)
+    // )
+    // object.response.clientDataJSON.challengeer = base64.decode(
+    //   object.response.clientDataJSON.challenge
+    // )
+    // object.response.isEqual =
+    //   object.response.clientDataJSON.challengeer === randomChallengeStr
+    //     ? true
+    //     : false
+
+    // // const parsedAttestObj = parseAttestationObject(
+    // //   object.response.attestationObject
+    // // )
+    // // object.response.parsedAttObject = parsedAttestObj
+    // object.response.attest = parseAttestationObject(
+    //   object.response.attestationObject
+    // )
+
+    res.json(userKeys)
+  } catch (e) {
+    res.json(e)
   }
-  const object = req.body
-  // object.rawIdconverted = decode(object.rawId, "utf-8")
-  // result.publicKey = object.id
-  result.clientDataJSON = JSON.parse(
-    base64.decode(object.response.clientDataJSON)
-  )
-  result.challenge = base64.decode(object.response.clientDataJSON.challenge)
-  result.isEqual = result.challenge === result.id ? true : false
-
-  // const parsedAttestObj = parseAttestationObject(
-  //   object.response.attestationObject
-  // )
-  // object.response.parsedAttObject = parsedAttestObj
-  result.attestationObject = parseAttestationObject(
-    object.response.attestationObject
-  )
-
-  userKeys.push(result)
-
-  // object.response.clientDataJSON = JSON.parse(
-  //   base64.decode(object.response.clientDataJSON)
-  // )
-  // object.response.clientDataJSON.challengeer = base64.decode(
-  //   object.response.clientDataJSON.challenge
-  // )
-  // object.response.isEqual =
-  //   object.response.clientDataJSON.challengeer === randomChallengeStr
-  //     ? true
-  //     : false
-
-  // // const parsedAttestObj = parseAttestationObject(
-  // //   object.response.attestationObject
-  // // )
-  // // object.response.parsedAttObject = parsedAttestObj
-  // object.response.attest = parseAttestationObject(
-  //   object.response.attestationObject
-  // )
-
-  res.json(userKeys)
 }
 
 const saveBuffer = (req, res) => {
