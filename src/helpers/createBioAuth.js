26a9932a22edfdeda1cf8c161d69eee8bb60b16c
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-14 12:26:56 +0300

    sd

--- src/helpers/createBioAuth.js
+++ src/helpers/createBioAuth.js
@@ -11,35 +11,36 @@ const createBioAuth = (req, res) => {
 
   keys.push(req.body)
 
-  // const data = req.body.data
+  const data = req.body.data
+  const username = req.body.userInfoforSession
 
-  // result.sessionUsername = req.body.userInfoforSession
-  // result.rawId = data.rawId
-  // result.userAgent = req.headers["user-agent"]
-  // result.publicKeywhichisID = data.id
-  // result.clientDataJSON = JSON.parse(
-  //   base64.decode(data.response.clientDataJSON)
-  // )
-  // result.challenge = base64.decode(result.clientDataJSON.challenge)
-  // result.isEqual = result.challenge === result.id ? true : false
-  // result.attestationObject = utils.parseAttestationObject(
-  //   data.response.attestationObject
-  // )
+  result.sessionUsername = username
+  result.rawId = data.rawId
+  result.userAgent = req.headers["user-agent"]
+  result.publicKeywhichisID = data.id
+  result.clientDataJSON = JSON.parse(
+    base64.decode(data.response.clientDataJSON)
+  )
+  result.challenge = base64.decode(result.clientDataJSON.challenge)
+  result.isEqual = result.challenge === result.id ? true : false
+  result.attestationObject = utils.parseAttestationObject(
+    data.response.attestationObject
+  )
 
-  // if (result.challenge !== database[sessionUsername].session.challenge) {
-  //   res.json({
-  //     status: "failed",
-  //     message: "Challenges don't match!",
-  //   })
-  // }
+  if (result.challenge !== database[username].session.challenge) {
+    res.json({
+      status: "failed",
+      message: "Challenges don't match!",
+    })
+  }
 
-  // if (clientDataJSON.origin !== "https://jade-brioche-7c33fd.netlify.app") {
-  //   res.json({
-  //     status: "failed",
-  //     message: "Origins dont match!",
-  //   })
-  // }
-  // database[result.sessionUsername].authenticators.push(result)
+  if (clientDataJSON.origin !== "https://jade-brioche-7c33fd.netlify.app") {
+    res.json({
+      status: "failed",
+      message: "Origins dont match!",
+    })
+  }
+  database[username].authenticators.push(result)
 
   res.json(database)
 }
