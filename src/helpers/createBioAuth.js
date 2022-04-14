b693dbac66e1525c35db5b1c82580caaad8c7e6a
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-14 12:15:22 +0300

    uioio

--- src/helpers/createBioAuth.js
+++ src/helpers/createBioAuth.js
@@ -1,6 +1,7 @@
 const base64 = require("base-64")
 const database = require("../store/bioUsersData.json")
 const utils = require("../utils")
+const keys = require("../store/keys.json")
 
 const createBioAuth = (req, res) => {
   const result = {
@@ -8,35 +9,37 @@ const createBioAuth = (req, res) => {
     counter: 0,
   }
 
-  const data = req.body.data
+  keys.push(req.body)
 
-  result.sessionUsername = req.body.userInfoforSession
-  result.rawId = data.rawId
-  result.userAgent = req.headers["user-agent"]
-  result.publicKeywhichisID = data.id
-  result.clientDataJSON = JSON.parse(
-    base64.decode(data.response.clientDataJSON)
-  )
-  result.challenge = base64.decode(result.clientDataJSON.challenge)
-  result.isEqual = result.challenge === result.id ? true : false
-  result.attestationObject = utils.parseAttestationObject(
-    data.response.attestationObject
-  )
+  // const data = req.body.data
 
-  if (result.challenge !== database[sessionUsername].session.challenge) {
-    res.json({
-      status: "failed",
-      message: "Challenges don't match!",
-    })
-  }
+  // result.sessionUsername = req.body.userInfoforSession
+  // result.rawId = data.rawId
+  // result.userAgent = req.headers["user-agent"]
+  // result.publicKeywhichisID = data.id
+  // result.clientDataJSON = JSON.parse(
+  //   base64.decode(data.response.clientDataJSON)
+  // )
+  // result.challenge = base64.decode(result.clientDataJSON.challenge)
+  // result.isEqual = result.challenge === result.id ? true : false
+  // result.attestationObject = utils.parseAttestationObject(
+  //   data.response.attestationObject
+  // )
 
-  if (clientDataJSON.origin !== "https://jade-brioche-7c33fd.netlify.app") {
-    res.json({
-      status: "failed",
-      message: "Origins dont match!",
-    })
-  }
-  database[result.sessionUsername].authenticators.push(result)
+  // if (result.challenge !== database[sessionUsername].session.challenge) {
+  //   res.json({
+  //     status: "failed",
+  //     message: "Challenges don't match!",
+  //   })
+  // }
+
+  // if (clientDataJSON.origin !== "https://jade-brioche-7c33fd.netlify.app") {
+  //   res.json({
+  //     status: "failed",
+  //     message: "Origins dont match!",
+  //   })
+  // }
+  // database[result.sessionUsername].authenticators.push(result)
 
   res.json(database)
 }
