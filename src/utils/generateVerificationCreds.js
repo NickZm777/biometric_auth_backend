c989d19910e85109f9a4bbc8ffd45c6b299ad831
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-20 14:33:14 +0300

    dfg

--- src/utils/generateVerificationCreds.js
+++ src/utils/generateVerificationCreds.js
@@ -1,25 +1,18 @@
-const nchallenge = require("crypto").randomBytes(16).toString("hex")
 const generateVerificationCreds = (publicKey) => {
   return {
-    // challenge: randomBase64URLBuffer(32),
-    //   rp: { name: "My test TouchID" },
-    // challenge: base64urlDecode(challenge),
-    challenge: nchallenge,
+    
+    challenge: "randomchallengefromgenerateServerVerificationCredRequest",
     rpId: "domain",
     allowCredentials: [
       {
         type: "public-key",
-        // id: base64urlDecode(publicKey),
+    
         id: publicKey,
-        // transports: ["internal"],
+        transports: ["internal"],
       },
     ],
     userVerification: "required",
-    //   authenticatorSelection: {
-    //     authenticatorAttachment: "platform",
-    //     userVerification: "required",
-    //   },
-  }
+ 
 }
 
 module.exports = generateVerificationCreds
