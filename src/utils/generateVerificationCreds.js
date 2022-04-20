66e74499dfeb7353e79fd9d118974df0a4155aa7
Змурщик Николай <m.zmurshyk@bpcbt.com>
2022-04-20 14:35:49 +0300

    iop

--- src/utils/generateVerificationCreds.js
+++ src/utils/generateVerificationCreds.js
@@ -1,18 +1,16 @@
 const generateVerificationCreds = (publicKey) => {
   return {
-    
     challenge: "randomchallengefromgenerateServerVerificationCredRequest",
     rpId: "domain",
     allowCredentials: [
       {
         type: "public-key",
-    
         id: publicKey,
         transports: ["internal"],
       },
     ],
     userVerification: "required",
- 
+  }
 }
 
 module.exports = generateVerificationCreds
